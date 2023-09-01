6a84de4d058d3dd6b93432b785fa1e9f5330bd83
peteherman <peteherman@yahoo.com>
2023-08-31 22:37:35 -0400

    added some stuff to prepare for connecting to hosts

--- playbook.go
+++ playbook.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/ssh"
 	"gopkg.in/yaml.v3"
 	"os"
-	"fmt"
 )
 
 type Playbook struct {
@@ -14,15 +17,15 @@ type Playbook struct {
 }
 
 type CommandTask struct {
-	Name    string `yaml:name`
-	Cmd string `yaml:cmd`
+	Name string `yaml:name`
+	Cmd  string `yaml:cmd`
 }
 
-type TaskResult interface {
-	Stdout() string
-	Stderr() string
-	ReturnCode() int
-}
+const (
+	NotInitiatedConnection = iota
+	SuccessfulConnection
+	FailedConnection
+)
 
 type PlaybookResult map[string]map[string]TaskResult
 
@@ -47,13 +50,88 @@ func playbookFromContents(contents []byte) (Playbook, error) {
 	return playbook, nil
 }
 
+type executingHost struct {
+	Host       *Host
+	connStatus int
+	conn       Connection
+}
+
+type Connection interface {
+	Connect(*Host) error
+	Run(string) (TaskResult, error)
+}
+
+type TaskResult interface {
+	Stdout() string
+	StdoutBytes() []byte
+	Stderr() string
+	StderrBytes() []byte
+	ReturnCode() int
+}
+
+type SSHConnection struct {
+	client *ssh.Client
+}
+
+func (s SSHConnection) Run(command string) (TaskResult, error) {
+	return SSHCommandResult{}, nil
+}
+
+func (s SSHConnection) Connect(host *Host) error {
+	return errors.New("Haven't implemented the ssh connection yet")
+}
+
+type SSHCommandResult struct {
+	stdoutBuffer bytes.Buffer
+	stderrBuffer bytes.Buffer
+	returnCode   int
+}
+
+func (s SSHCommandResult) StdoutBytes() []byte {
+	return s.stdoutBuffer.Bytes()
+}
+
+func (s SSHCommandResult) StderrBytes() []byte {
+	return s.stderrBuffer.Bytes()
+}
+
+func (s SSHCommandResult) ReturnCode() int {
+	return s.returnCode
+}
+
+func (s SSHCommandResult) Stdout() string {
+	return s.stdoutBuffer.String()
+}
+
+func (s SSHCommandResult) Stderr() string {
+	return s.stderrBuffer.String()
+}
+
 func (p Playbook) Execute(inventory Inventory) PlaybookResult {
 
-	executionHosts := inventory.ExecutionHosts(p.Hosts)
+	hosts := inventory.ExecutionHosts(p.Hosts)
+	executionHosts := make(map[string]executingHost, len(hosts))
+	for _, host := range hosts {
+		hostAddress := fmt.Sprintf("%v", host)
+		executionHosts[hostAddress] = executingHost{
+			Host:       host,
+			connStatus: NotInitiatedConnection,
+			conn:       SSHConnection{},
+		}
+	}
 	for _, task := range p.Tasks {
-
-		for _, host := range executionHosts {
-			fmt.Printf("%v should execute this: %v\n", host.name, task)
+		for _, host := range hosts {
+			executionHost := executionHosts[fmt.Sprintf("%v", host)]
+			if executionHost.connStatus == FailedConnection {
+				continue
+			} else if executionHost.connStatus == NotInitiatedConnection {
+				if err := executionHost.conn.Connect(executionHost.Host); err != nil {
+					fmt.Printf("Error connecting to host: %v\n", host.name)
+					executionHost.connStatus = FailedConnection
+					continue
+				}
+			}
+			executionHost.conn.Run(task.Cmd)
 		}
 	}
 	return make(PlaybookResult, 0)
