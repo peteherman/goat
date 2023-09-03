da651c9f713fc55c6f4917dddc0ab4b32f4efd7a
peteherman <peteherman@yahoo.com>
2023-09-03 10:28:19 -0400

    separated ssh connection functionality from playbook

---
+++ ssh.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/ssh"
+)
+
+type SSHConnection struct {
+	Client    *ssh.Client
+	connError error
+}
+
+func (s *SSHConnection) SetConnectionError(err error) {
+	s.connError = err
+}
+
+func (s *SSHConnection) Status() int {
+	if s.connError == nil && s.Client == nil {
+		return NotInitiatedConnection
+	}
+	if s.connError == nil && s.Client != nil {
+		return SuccessfulConnection
+	}
+	return FailedConnection
+}
+
+func (s *SSHConnection) Run(command string) (TaskResult, error) {
+	if status := s.Status(); status != SuccessfulConnection {
+		if status == FailedConnection {
+			return SSHCommandResult{}, errors.New("Connection failed")
+		}
+		return SSHCommandResult{}, errors.New("Connection not initiated")
+	}
+	session, err := s.Client.NewSession()
+	if err != nil {
+		s.connError = err
+		return SSHCommandResult{}, errors.New("Unable to create session on host\n")
+	}
+	defer session.Close()
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	session.Stdout = &stdout
+	session.Stderr = &stderr
+	err = session.Run(command)
+
+	return SSHCommandResult{
+		stdoutBuffer: stdout,
+		stderrBuffer: stderr,
+		returnCode:   0,
+	}, nil
+}
+
+func (s *SSHConnection) Connect(host *Host) error {
+	username, keyExists := host.Vars["username"]
+	if !keyExists {
+		return errors.New(fmt.Sprintf("Cannot connect to host %v, no username provided\n",
+			host.name))
+	}
+	password, keyExists := host.Vars["password"]
+	if !keyExists {
+		return errors.New(fmt.Sprintf("Cannot connect to host %v, no password provided\n",
+			host.name))
+	}
+
+	config := &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	address := host.name
+	if newAddress, keyExists := host.Vars["address"]; keyExists {
+		address = newAddress
+	}
+
+	port := "22"
+	if specifiedPort, keyExists := host.Vars["port"]; keyExists {
+		port = specifiedPort
+	}
+
+	uri := fmt.Sprintf("%v:%v", address, port)
+
+	client, err := ssh.Dial("tcp", uri, config)
+	if err != nil {
+		return errors.New(fmt.Sprintf("Error when connecting to host: %v\n", err))
+	}
+	s.Client = client
+
+	return nil
+}
+
+type SSHCommandResult struct {
+	stdoutBuffer bytes.Buffer
+	stderrBuffer bytes.Buffer
+	returnCode   int
+}
+
+func (s SSHCommandResult) StdoutBytes() []byte {
+	return s.stdoutBuffer.Bytes()
+}
+
+func (s SSHCommandResult) StderrBytes() []byte {
+	return s.stderrBuffer.Bytes()
+}
+
+func (s SSHCommandResult) ReturnCode() int {
+	return s.returnCode
+}
+
+func (s SSHCommandResult) Stdout() string {
+	return s.stdoutBuffer.String()
+}
+
+func (s SSHCommandResult) Stderr() string {
+	return s.stderrBuffer.String()
+}
