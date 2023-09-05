3ce68b9527b5733ad6af2e66d43197bb91c14290
peteherman <peteherman@yahoo.com>
2023-09-05 06:12:20 -0400

    improved output

--- output.go
+++ output.go
@@ -1,34 +1,53 @@
 package main
 
 import (
-	"strings"
+	"bufio"
 	"fmt"
+	"strings"
 )
 
 type OutputFormatter interface {
 	Output(string, string, TaskResult) string
 }
 
-type StdoutFormatter struct {}
+type StdoutFormatter struct{}
 
 func (s StdoutFormatter) Output(taskName, hostname string, result TaskResult) string {
 
-	var sb strings.Builder	
+	var sb strings.Builder
 	sb.WriteString(fmt.Sprintf("task: %v\n", taskName))
 	sb.WriteString(fmt.Sprintf("\thost: %v\n", hostname))
 
 	taskErr := result.Error()
 	if taskErr != nil {
 		sb.WriteString(fmt.Sprintf("\t\terror: %v\n", taskErr.Error()))
+		stdout := result.Stdout()
+		scanner := bufio.NewScanner(strings.NewReader(stdout))
+		sb.WriteString(fmt.Sprintf("\t\tstdout:\n"))
+		for scanner.Scan() {
+			sb.WriteString(fmt.Sprintf("\t\t\t%v\n", strings.TrimSpace(scanner.Text())))
+		}
+		stderr := result.Stderr()		
+		scanner = bufio.NewScanner(strings.NewReader(stderr))
+		sb.WriteString(fmt.Sprintf("\t\tstderr:\n"))
+		for scanner.Scan() {
+			sb.WriteString(fmt.Sprintf("\t\t\t%v\n", strings.TrimSpace(scanner.Text())))
+		}
 		return sb.String()
 	}
-	
+
 	stdout := result.Stdout()
-	sb.WriteString(fmt.Sprintf("\t\tstdout: %v\n", strings.TrimSpace(stdout)))
-	stderr := result.Stderr()
-	sb.WriteString(fmt.Sprintf("\t\tstderr: %v\n", strings.TrimSpace(stderr)))
+	scanner := bufio.NewScanner(strings.NewReader(stdout))
+	sb.WriteString(fmt.Sprintf("\t\tstdout:\n"))
+	for scanner.Scan() {
+		sb.WriteString(fmt.Sprintf("\t\t\t%v\n", strings.TrimSpace(scanner.Text())))
+	}
+	stderr := result.Stderr()	
+	scanner = bufio.NewScanner(strings.NewReader(stderr))
+	sb.WriteString(fmt.Sprintf("\t\tstderr:\n"))
+	for scanner.Scan() {
+		sb.WriteString(fmt.Sprintf("\t\t\t%v\n", strings.TrimSpace(scanner.Text())))
+	}
 	return sb.String()
-	
+
 }
-
-
